9ba15a7b11918d0239e664f6e5b64a0f97912c14
marat-chardymov <marat.chardymov@gmail.com>
2014-02-12 21:53:02 +0300

    init commit, news entity added

---
+++ src/main/java/com/epam/model/entities/AbstractEntity.java
@@ -0,0 +1,13 @@
+package com.epam.model.entities;
+
+public class AbstractEntity {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/epam/model/entities/News.java
@@ -0,0 +1,13 @@
+package com.epam.model.entities;
+
+public class News extends AbstractEntity {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
