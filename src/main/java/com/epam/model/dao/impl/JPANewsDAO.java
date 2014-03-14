32c867eeeae4a0b4d66deec3685d659c7e3cfc10
marat-chardymov <marat.chardymov@gmail.com>
2014-03-14 19:09:53 +0300

    JPANewsDAO delete() and deleteList() are implemented

--- src/main/java/com/epam/model/dao/impl/JPANewsDAO.java
+++ src/main/java/com/epam/model/dao/impl/JPANewsDAO.java
@@ -3,22 +3,21 @@ package com.epam.model.dao.impl;
 import com.epam.exceptions.AppDAOException;
 import com.epam.model.dao.INewsDAO;
 import com.epam.model.entities.News;
+import com.google.common.primitives.Ints;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 @Transactional
-public class JPANewsDAO implements INewsDAO{
+public class JPANewsDAO implements INewsDAO {
 
-    private static final String FIND_All_SQL="SELECT n FROM News n";
-    private static final String FIND_All_SQL="DELETE n FROM News n";
+    private static final String FIND_All = "SELECT n FROM News n";
+    private static final String DELETE = "DELETE FROM News news WHERE news.id= :id";
+    private static final String DELETE_LIST = "DELETE News news WHERE news.id IN (:idList)";
 
     @PersistenceContext
     private EntityManager em;
@@ -30,7 +29,7 @@ public class JPANewsDAO implements INewsDAO{
 
     @Override
     public News read(int id) throws AppDAOException {
-        return em.find(News.class,id);
+        return em.find(News.class, id);
     }
 
     @Override
@@ -40,16 +39,21 @@ public class JPANewsDAO implements INewsDAO{
 
     @Override
     public void delete(int id) throws AppDAOException {
-      
+        Query query = em.createQuery(DELETE);
+        query.setParameter("id", id);
+        query.executeUpdate();
     }
 
     @Override
     public void deleteList(int[] ids) throws AppDAOException {
-
+        Query query = em.createQuery(DELETE_LIST);
+        List<Integer> idList = Ints.asList(ids);
+        query.setParameter("idList", idList);
+        query.executeUpdate();
     }
 
     @Override
     public List<News> findAll() throws AppDAOException {
-        return em.createQuery(FIND_All_SQL).getResultList();
+        return em.createQuery(FIND_All).getResultList();
     }
 }
